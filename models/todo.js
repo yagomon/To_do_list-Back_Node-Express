0b264e31e769ee0aa44532c429e967a41b64f1ba
yago.monteiroo@gmail.com <yago.monteiroo@gmail.com>
2021-09-15 22:19:48 -0300

    schema

--- models/todo.js
+++ models/todo.js
@@ -3,12 +3,12 @@ const mongoose = require('mongoose');
 
 
 const todoModel = new mongoose.Schema({
-    título:{type: String, required: true},
-    descrição:{type: String},
+    titulo:{type: String, required: true},
+    descricao:{type: String},
     prioridade:{type: String, required: true},
     status:{type: String, required:true},
     prazo:{type: String},
-    dataCriação:{type: Date, default: Date.now}
+    dataCriacao:{type: Date, default: Date.now}
 });
 
 const Todo = mongoose.model("tarefas",todoModel);
